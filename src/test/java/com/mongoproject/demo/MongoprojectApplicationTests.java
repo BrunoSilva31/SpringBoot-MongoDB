7607b139d6544ae73d2995c5f7ca695b08446523
BrunoSilva31 <brunosilva9725@gmail.com>
2024-10-16 17:18:11 -0300

    First commit

---
+++ src/test/java/com/mongoproject/demo/MongoprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mongoproject.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
