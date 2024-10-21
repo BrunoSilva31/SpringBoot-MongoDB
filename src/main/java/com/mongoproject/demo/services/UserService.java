4b75b7cf7ba8051db0a0f487eb5348d3e131f732
BrunoSilva31 <brunosilva9725@gmail.com>
2024-10-21 17:35:51 -0300

    findById

--- src/main/java/com/mongoproject/demo/services/UserService.java
+++ src/main/java/com/mongoproject/demo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.mongoproject.demo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mongoproject.demo.domain.User;
 import com.mongoproject.demo.repository.UserRepository;
+import com.mongoproject.demo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,6 +19,15 @@ public class UserService {
     public List<User> findAll(){
         return repo.findAll();
     }
+
+
+    public User findById(String id) {
+
+        Optional<User> user = repo.findById(id);
+
+        return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
+    }
+
 
     
 }
